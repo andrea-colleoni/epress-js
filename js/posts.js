e541dd3fae5515f27d55dae22920b8f48eb82a24
Andrea Colleoni <andrea@colleoni.info>
2018-02-15 14:52:51 +0100

    aggiunta bookmarks

--- js/posts.js
+++ js/posts.js
@@ -15,9 +15,16 @@ $(function() {  // closure
             generaTuttiPost(templPost, templCommento);
         });
     });
+    $( "#postsContainer" ).on("click", "span.not-starred", function() {
+        aggiungiBookmark( $( this ) );
+    });
+    $( "#postsContainer" ).on("click", "span.starred", function() {
+        rimuoviBookmark( $( this ) );
+    });
     console.log('finito');
 });
 
+/* giorno 1 */
 function generaTuttiPost(templPost, templCommento) {
    // punto 3.
    $.ajax({
@@ -25,9 +32,9 @@ function generaTuttiPost(templPost, templCommento) {
         method: 'get'
     })
     .done(function(listaPosts) {
-        console.log(listaPosts);
+        //console.log(listaPosts);
         $.each(listaPosts, function(ixPost, post) {
-            console.log(post);
+            //console.log(post);
             generaSingoloPost(post, templPost, templCommento);
         });
     }); 
@@ -43,6 +50,15 @@ function generaSingoloPost(post, templPost, templCommento) {
     );
     commentiDelPost(post.id, templCommento);
     impostaAutoreDelPost(post);
+
+    var arr = caricaBookmarks();
+    if(arr.indexOf(post.id) >= 0) {
+        $('span[data-postid=' + post.id + ']').removeClass('not-starred');
+        $('span[data-postid=' + post.id + ']').addClass('starred');
+    } else {
+        $('span[data-postid=' + post.id + ']').removeClass('starred');
+        $('span[data-postid=' + post.id + ']').addClass('not-starred');
+    }
 }
 
 function impostaAutoreDelPost(post) {
@@ -51,7 +67,8 @@ function impostaAutoreDelPost(post) {
         method: 'get'
     })
     .done(function (user) {
-        $('#autore-' + post.id).html('<span class="oi oi-person"></span> ' + user.name);
+        $('#autore-' + post.id + ' .nome-autore').html(user.name);
+        //$('#autore-' + post.id).html('<span class="oi oi-person"></span> ' + user.name);
         $('#autore-' + post.id).attr('href', 'utente.html?userId=' + user.id);
     });
 }
@@ -74,4 +91,47 @@ function commentiDelPost(postId, templCommento) {
             );
         });
     }); 
+}
+
+/* giorno 2 */
+function caricaBookmarks() {
+    var strPreferiti = localStorage.getItem('postPreferiti');
+    var bookmarks = (strPreferiti ? JSON.parse(localStorage.getItem('postPreferiti')) : []);
+    return bookmarks;
+}
+
+function salvaBookmarks(arr) {
+    localStorage.setItem('postPreferiti', JSON.stringify(arr));
+}
+
+function eliminaTuttiIBookmarks() {
+    localStorage.removeItem('postPreferiti');
+}
+
+function aggiungiBookmark( starElement ) {
+    var postId = new Number(starElement.data('postid'));
+    var arr = caricaBookmarks();
+    if (arr.indexOf(postId.valueOf()) >= 0) {
+        console.warn('bookmark già esistente');
+    } else {
+        arr.push(postId);
+        starElement.removeClass('not-starred');
+        starElement.addClass('starred');
+        salvaBookmarks(arr);
+        console.log('bookmark aggiunto');
+    }
+}
+
+function rimuoviBookmark( starElement ) {
+    var postId = new Number(starElement.data('postid'));
+    var arr = caricaBookmarks();
+    if (arr.indexOf(postId.valueOf()) >= 0) {
+        arr.splice(arr.indexOf(postId.valueOf()), 1);
+        starElement.removeClass('starred');
+        starElement.addClass('not-starred');
+        salvaBookmarks(arr);
+        console.log('bookmark aggiunto');     
+    } else {
+        console.warn('bookmark inesistente');
+    }
 }
